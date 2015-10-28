1c233cad065c9d7e4d7c3d50b5b2d09125c06d75
Lars Paulson <larspcus2@gmail.com>
2015-10-28 10:38:54 -0700

    Add university numbers

--- www/module/footer.php
+++ www/module/footer.php
@@ -10,8 +10,10 @@
       </div> <!-- footer_images_container -->
       
       <div id="footer_about">
-        <div id="footer_copyright">
-        OSUndead &copy <?php echo date("Y"); ?>
+      <div id="footer_Design">
+          Counseling Services: 541-737-2131<br>
+          DPS non-emergency: 541-737-3010<br>
+          DPS emergency: 541-737-7000 <br>
         </div>
         <div id="footer_design">
         Using the <a class="footer" href="https://github.com/rylinks/knoopvszombies">KnoopVsZombies</a> game engine by <a class="footer" href="http://mikeknoop.com">Mike Knoop</a>
